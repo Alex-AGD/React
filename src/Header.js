6d58565bc33673b5c532f23289ab56a3887d0433
Aliaksei Dauhapolau <aliaksei.dauhapolau@gmail.com>
2020-09-16 19:15:26 +0300

    1st commint

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+            <ul>
+                <a href='!#'>Two component</a> -
+                <a href='!#'>This</a> -
+                <a href='!#'>Ass</a> -
+                <a href='!#'>React</a> -
+            </ul>
+        </div>
+    );
+}
+export default Header;
---
+++ src/Technologies.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Technologies = () => {
+    return (
+        <ul>
+            <li>Simple HTML</li>
+            <li>CSS</li>
+            <li>Js</li>
+            <li>React</li>
+        </ul>
+    );
+}
+
+export default Technologies;
