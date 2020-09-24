7ac4d015fe266bea93bff390af65353fa9a2a420
Aliaksei Dauhapolau <aliaksei.dauhapolau@gmail.com>
2020-09-24 15:16:27 +0300

    Модули CSS, импорты, Бем

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import './Header.module.css'
+import h from './Header.module.css'
 
 const Header = () => {
-    return <header className='header'>
+    return <header className={h.header}>
         <img src="https://n1.nextpng.com/sticker-png/570/557/sticker-png-react-logo-redux-javascript-vuejs-babel-nodejs-npm-web-application.png"/>
     </header>
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,24 +1,22 @@
 import React from "react";
+import './Profile.module.css';
+import p from './Profile.module.css'
 
 const Profile = () => {
-    return <div className='content'>
+    return <div className={p.content}>
         <img src='https://images.unsplash.com/photo-1452805809447-a55bbdbc76ae?ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80'/>
-        <div>
-            avatar + description
+
+        <div className={p.item}>
+        <div>My posts<div>
+            <div>New Post</div>
         </div>
 
-        <div>
-            My posts
-            <div>
-                New Post
-            </div>
-        </div>
+        <div className={p.item}>
+            <div>post 1</div>
+            <div>post 2</div>
 
-        <div>
-            post 1
         </div>
-        <div>
-            post 2
+        </div>
         </div>
     </div>
 
