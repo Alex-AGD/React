1216fe22f17269ea7d607b3dee0a329fe91d8476
Aliaksei Dauhapolau <aliaksei.dauhapolau@gmail.com>
2020-10-18 20:27:34 +0300

    Bootstrap (css)

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import './Profile.module.css';
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
+import s from "./Profile.module.css"
 
 const Profile = (props) => {
 
     return <div>
-        <ProfileInfo  />
-        <MyPostsContainer store={props.store}/>
+
+        <div className={s.layer1}> <ProfileInfo  /> </div>
+        <div className={s.layer2}><MyPostsContainer store={props.store}/> </div>
+
     </div>
 }
 
