af0c1b176a7924dcc018969937ba060fc7fcc3f8
Aliaksei Dauhapolau <aliaksei.dauhapolau@gmail.com>
2020-10-29 21:36:02 +0300

    классовый компонент (extends React.Component)

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ const App = (props) => {
             </div>
         </div>
     )
+
 }
 
 export default App;
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,65 +1,50 @@
 import React from "react";
-import styles from './Users.module.css'
+import userAvatar from "../../assets/images/icons-user.png";
+import styles from "./Users.module.css";
+import * as axios from "axios";
 
-const AVATAR = 'https://www.joho.se/wp-content/uploads/2017/11/cropped-samurai-ninja-avatar-warrior-icon-1.png';
 
-let Users = (props) => {
-    if (props.users.length === 0) {
-        props.setUsers ([
-                {
-                    id: 1,
-                    photoURL: AVATAR,
-                    followed: false,
-                    fullName: 'Alex',
-                    status: 'Boss',
-                    location: { city: 'Gomel', country: 'Belarus' }
-                },
-                {
-                    id: 2,
-                    photoURL: AVATAR,
-                    followed: true,
-                    fullName: 'Jim',
-                    status: 'User',
-                    location: { city: 'Kiev', country: 'Ukraine' }
-                },
-                {
-                    id: 3,
-                    photoURL: AVATAR,
-                    followed: true,
-                    fullName: 'Janet',
-                    status: 'User',
-                    location: { city: 'Idaho', country: 'USA' }
-                }
-            ]
-        )
+class Users extends React.Component {
+    constructor (props) {
+        super (props);
+            axios.get ("https://social-network.samuraijs.com/api/1.0/users")
+                .then (response => {
+                    this.props.setUsers (response.data.items);
+                });
+
     }
-
-    return <div>
-        {
-            props.users.map (u => <div key={ u.id }>
+    render () {
+        return <div>
+            {
+                this.props.users.map (u => <div key={ u.id }>
                 <span>
                     <div>
-                        <img src={ u.photoURL } className={ styles.userPhoto }/>
+                        {/*<img src={ u.photos.small != null ? u.photos.small : userAvatar }*/ }
+                        <img src={ userAvatar } className={ styles.userPhoto }/>
                     </div>
                     <div>
                             { u.followed ?
-                                <button onClick={ () => props.unfollow (u.id) }>Unfollow</button> :
-                                <button onClick={ () => props.follow (u.id) }>Follow</button> }
+                                <button onClick={ () => this.props.unfollow (u.id) }>Unfollow</button> :
+                                <button onClick={ () => this.props.follow (u.id) }>Follow</button> }
                     </div>
                 </span>
-                <span>
                     <span>
-                        <div>{ u.fullName }</div>
+                    <span>
+                        <div>{ u.name }</div>
                         <div>{ u.status }</div>
+                        <div>{ u.email }</div>
                     </span>
                     <span>
-                 {/*       <div>{ u.location.country }</div>
-                        <div>{ u.location.city }</div>*/}
+                        <div>{ "u.location.country" }</div>
+                        <div>{ "u.location.city" }</div>
                     </span>
                 </span>
-            </div>)
-        }
-    </div>
+                </div>)
+            }
+        </div>
+    }
+
 }
+
 
 export default Users
