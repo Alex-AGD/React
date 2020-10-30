65d6dc15823e841049a2f48310441c5c26efd780
Aliaksei Dauhapolau <aliaksei.dauhapolau@gmail.com>
2020-10-30 19:47:34 +0300

    pagination, постраничный вывод пользователей

--- src/components/Users/UserContainer.jsx
+++ src/components/Users/UserContainer.jsx
@@ -1,30 +1,37 @@
 import React from "react";
 import Users from "./Users";
 import connect from "react-redux/lib/connect/connect";
-import { followAC, setUsersAC, unfollowAC } from "../../redux/usersReducer";
+import { followAC, setCurrentPageAC, setUsersAC, setUsersTotalCountAC, unfollowAC } from "../../redux/usersReducer";
 
 
 let mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage,
     }
 }
 
 let mapDispatchToProps = (dispatch) => {
     return {
         follow: (userId) => {
-            dispatch (followAC (userId))
+            dispatch (followAC (userId));
         },
         unfollow: (userId) => {
-            dispatch (unfollowAC (userId))
+            dispatch (unfollowAC (userId));
         },
         setUsers: (users) => {
-            dispatch (setUsersAC (users))
+            dispatch (setUsersAC (users));
+        },
+        setCurrentPage: (pageNumber) => {
+            dispatch (setCurrentPageAC (pageNumber))
+        },
+        setTotalUsersCount: (totalCount) => {
+            dispatch (setUsersTotalCountAC (totalCount))
         }
     }
 }
 
+export default connect (mapStateToProps, mapDispatchToProps) (Users);
 
-const UserContainer = connect (mapStateToProps, mapDispatchToProps) (Users);
-
-export default UserContainer;
