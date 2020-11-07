caaa3873d0c2b5a4d71caa3e0d22a5095fdab4df
Aliaksei Dauhapolau <aliaksei.dauhapolau@gmail.com>
2020-11-07 18:02:52 +0300

    test

--- src/components/common/Preloader/Preloader.js
+++ src/components/common/Preloader/Preloader.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import preloader from "../../../assets/images/preloader.svg";
+import preloader from '../../../assets/images/preloader.svg'
+
+const PreloaderIcon = preloader;
 
 let Preloader = (props) => {
     return  (
-        <img src={preloader} />
+        <img src={PreloaderIcon} />
     )}
 
 export default Preloader;
